9d022ee170c7b33f30e2e93e3f1a01126d874993
TheDarkRex <erykmasian@gmail.com>
2025-12-18 23:09:53 +0100

    init commit for rest package

--- src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
@@ -1,45 +1,30 @@
 package pl.put.poznan.sqc.rest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Arrays;
-
+import pl.put.poznan.sqc.model.Scenario;
+import pl.put.poznan.sqc.logic.vistor.StepCounterVisitor;
 
 @RestController
-@RequestMapping("/{text}")
-public class TextTransformerController {
+@RequestMapping("/api") // Zmieniamy ścieżkę na bardziej standardową
+public class ScenarioQualityCheckerController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
+    private static final Logger logger = LoggerFactory.getLogger(ScenarioQualityCheckerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String get(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+    // Przykład endpointu do liczenia kroków
+    // Oczekuje JSONa ze scenariuszem w ciele żądania (Body)
+    @PostMapping("/calculate/steps")
+    public int calculateSteps(@RequestBody Scenario scenario) {
+        logger.debug("Received request to count steps for scenario: {}", scenario.getTitle());
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
+        // 1. Tworzymy wizytatora
+        StepCounterVisitor visitor = new StepCounterVisitor();
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
+        // 2. Uruchamiamy wizytatora na scenariuszu
+        scenario.accept(visitor);
+
+        // 3. Zwracamy wynik
+        return visitor.getStepCount();
     }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@PathVariable String text,
-                      @RequestBody String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
-    }
-
-
-
 }
-
-
