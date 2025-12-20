09535f9212c47aea8278d3f9a7d299c1e866ec68
kwonbx <zosia1811@op.pl>
2025-12-20 20:37:58 +0100

    Add ActorCheck and KeywordCounter visitors

---
+++ src/main/java/pl/put/poznan/sqc/logic/vistor/ActorCheckVisitor.java
@@ -0,0 +1,15 @@
+package pl.put.poznan.sqc.logic.vistor;
+import pl.put.poznan.sqc.model.Scenario;
+import pl.put.poznan.sqc.model.Step;
+
+public class ActorCheckVisitor implements Visitor{
+    @Override
+    public void visit(Scenario scenario) {
+
+    }
+
+    @Override
+    public void visit(Step step) {
+
+    }
+}
