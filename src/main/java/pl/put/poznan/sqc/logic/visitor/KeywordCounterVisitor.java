e77ed1a6b2f1a179b8ad37f22f2befc36e9ea3f8
Jakub Biernat <k.biernat@vp.pl>
2025-12-21 16:01:52 +0100

    Add documentation for KeywordCounterVisitor

--- src/main/java/pl/put/poznan/sqc/logic/visitor/KeywordCounterVisitor.java
+++ src/main/java/pl/put/poznan/sqc/logic/visitor/KeywordCounterVisitor.java
@@ -3,14 +3,32 @@ package pl.put.poznan.sqc.logic.visitor;
 import pl.put.poznan.sqc.model.Scenario;
 import pl.put.poznan.sqc.model.Step;
 
+/**
+ * Klasa implementująca interfejs {@link Visitor}, która
+ * zllicza kroki ({@link Step}) scenariusza {@link Scenario}
+ * rozpoczynające się od słów kluczowych.
+ */
+
 public class KeywordCounterVisitor implements Visitor {
+    /** Liczba kroków rozpoczynających się od słów kluczowych. */
     private int keywordCount = 0;
 
+    /**
+     * Metoda odwiedzająca scenariusz.
+     *
+     * @param scenario scenariusz do odwiedzenia
+     */
     @Override
     public void visit(Scenario scenario) {
 
     }
 
+    /**
+     * Metoda odwiedzająca krok scenariusza.
+     * Inkrementuje licznik, jeśli krok zaczyna się od słowa kluczowego.
+     *
+     * @param step krok do odwiedzenia
+     */
     @Override
     public void visit(Step step) {
         String content = step.getContent();
@@ -22,6 +40,11 @@ public class KeywordCounterVisitor implements Visitor {
         }
     }
 
+    /**
+     * Zwraca liczbę kroków rozpoczynających się od słów kluczowych.
+     *
+     * @return liczba kroków zaczynających się od słów kluczowych
+     */
     public int getKeywordCount() {
         return keywordCount;
     }
