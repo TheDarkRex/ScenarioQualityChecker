98282ac7067011298a8fae7a9abc63a61d368921
Dominik Durlik <59211452+domelan10@users.noreply.github.com>
2026-02-01 19:59:22 +0100

    Add step numbering and level filtering endpoints

--- src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/sqc/rest/ScenarioQualityCheckerController.java
@@ -2,16 +2,18 @@ package pl.put.poznan.sqc.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
 import pl.put.poznan.sqc.logic.RemoteXmlService;
 import pl.put.poznan.sqc.logic.visitor.ActorCheckVisitor;
 import pl.put.poznan.sqc.logic.visitor.KeywordCounterVisitor;
+import pl.put.poznan.sqc.logic.visitor.LevelFilterVisitor;
 import pl.put.poznan.sqc.logic.visitor.StepCounterVisitor;
+import pl.put.poznan.sqc.logic.visitor.StepNumberingVisitor;
 import pl.put.poznan.sqc.model.Scenario;
 
 /**
@@ -109,5 +111,34 @@ public class ScenarioQualityCheckerController {
     public String getScenarioAsXml(@RequestBody Scenario scenario) {
         logger.info("Received request to convert scenario to XML: {}", scenario.getTitle());
         return remoteXmlService.convertScenarioToXml(scenario);
+    }
+
+    @PostMapping(value = "/get-numbered-scenario")
+    public String getStepNumberingVisitor(@RequestBody Scenario scenario) {
+        logger.info("Received request to list all steps from scenario: {}", scenario.getTitle());
+        StepNumberingVisitor visitor = new StepNumberingVisitor();
+        scenario.accept(visitor);
+        return visitor.getScenarioText();
+    }
+
+    public static class ScenarioFilterRequest {
+        private Scenario scenario;
+        private int maxLevel;
+
+        public Scenario getScenario() { return scenario; }
+        public void setScenario(Scenario scenario) { this.scenario = scenario; }
+        public int getMaxLevel() { return maxLevel; }
+        public void setMaxLevel(int maxLevel) { this.maxLevel = maxLevel; }
+    }
+
+    @PostMapping(value = "/get-scenario-to-level")
+    public Scenario getStepNumberingVisitor(@RequestBody ScenarioFilterRequest request) {
+        Scenario scenario = request.getScenario();
+        int maxLevel = request.getMaxLevel();
+
+        logger.info("Received request to get limited scenario: {}, to a level of: {}", scenario.getTitle(), maxLevel);
+        LevelFilterVisitor visitor = new LevelFilterVisitor();
+        scenario.accept(visitor);
+        return visitor.getFilteredScenario();
     }
 }
